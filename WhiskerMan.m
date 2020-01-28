e781580a1615832dae753d3c4c4f55a32043abc3
Gerard Capes <gerard.capes@manchester.ac.uk>
2020-01-28 09:18:39 +0000

    Remove asserts and vectorization checks

--- WhiskerMan.m
+++ WhiskerMan.m
@@ -2701,17 +2701,14 @@ function [fp3, fidx, p, sfp] = find_follicle_poly_wm4a(r3,dt,folliclemask)
     end
     
     dist = zeros(length(sneg),size(folliclemask,2));
-    x_vec = polyval(p(1, :), sneg(:)) .';
-    y_vec = polyval(p(2, :), sneg(:)) .';
+    x = polyval(p(1, :), sneg(:)) .';
+    y = polyval(p(2, :), sneg(:)) .';
     for i = 1:length(sneg)
         for j = 1:size(folliclemask,2)
-            x = polyval(p(1,:),sneg(i));
-            y = polyval(p(2,:),sneg(i));
-            assert(isequal(x, x_vec(i)))
-            assert(isequal(y, y_vec(i)))
-            dist(i,j) = sum(([x;y]-folliclemask(:,j)).^2,1);    %distance in the H projection
+            dist(i, j) = sum(([x(i); y(i)] - folliclemask(:,j)).^2, 1);    %distance in the H projection
         end
     end
+    
     clear i j x y
     [m,fidx] = min(dist');  % min over folliclemask for fixed s
     % locate first minimum (in order of decreasing t)
